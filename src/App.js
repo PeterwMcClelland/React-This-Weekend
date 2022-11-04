732d8713c8e2be383ce648f73b88cb4e68bb7b11
Peter McClelland <peterwmcclelland@gmail.com>
2022-11-04 12:33:11 -0700

    search bar template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './components/Header';
+import Search from './components/SearchBar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
       <Header></Header>
+    <body>
+      <Search></Search>
+    </body>
     </div>
   );
 }
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 function Search() {
     
     return (
-        <div>
-            
-        </div>
-    )
+        <>
+        <InputGroup size="lg">
+        <InputGroup.Text id="inputGroup-sizing-lg">Search</InputGroup.Text>
+        <Form.Control
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+        />
+      </InputGroup>
+        </>
+    );
 }
+
+export default Search;
