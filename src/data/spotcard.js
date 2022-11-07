fa54945cff46d01d1788eb1d0521de7a503b5325
Peter McClelland <peterwmcclelland@gmail.com>
2022-11-06 17:39:14 -0800

    data

---
+++ src/data/spotcard.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const spotcard = [
+    {
+       id: 1,
+       imgPath: "./assets/images/beavertonpark.jpeg",
+       name: "Beaverton Skate Park",
+       address: "NW Schlottman Pl, Beaverton, OR 97006",
+    },
+    {
+
+    }
+]
