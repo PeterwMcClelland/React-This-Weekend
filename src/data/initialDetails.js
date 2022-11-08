8897a1d0a675a9810e4b2483e90b45e95d0ebfbb
Peter McClelland <peterwmcclelland@gmail.com>
2022-11-08 12:33:28 -0800

    new spots

--- src/data/initialDetails.js
+++ src/data/initialDetails.js
@@ -4,7 +4,7 @@ const initialDetails = [
     {
        id: 1,
        imgPath: "./assets/img/beavertonpark.jpeg",
-       name: "Beaverton Skate Park",
+       name: "Beaverton (Skate Park)",
        address: "Address: NW Schlottman Pl, Beaverton, OR 97006",
     },
     {
@@ -16,14 +16,32 @@ const initialDetails = [
     {
         id: 3,
         imgPath: "./assets/img/Burnside.png",
-        name: "Burnside Skate Park",
+        name: "Burnside (Skate Park)",
         address: "Address: SE 2nd Ave, Portland, OR 97232",
     },
     {
         id: 4,
         imgPath: "./assets/img/capitalhill9.png",
-        name: "Capital Hill 9",
+        name: "Capital Hill 9 (Rail)",
         address: "Address: 8401 SW 17th Ave, Portland, OR 97219, USA",
+    },
+    {
+        id: 5,
+        imgPath: "./assets/img/catspaw-004.jpeg",
+        name: "Cats Paw Saloon (Bar)",
+        address: "Address: 3565 SE Division St, Portland, OR 97202",
+    },
+    {
+        id: 6,
+        imgPath: "./assets/img/church9stair.png",
+        name: "Church 9 (Stairs)",
+        address: "Address: NW Everett & 19th, Portland, OR 97209, USA",
+    },
+    {
+        id: 7,
+        imgPath: "./assets/img/Crestonledge.png",
+        name: "Creston Ledge (Ledge)",
+        address: "Address: 4701 SE Bush St, Portland, OR 97206, USA",
     },
     // {
     //     id: ,
