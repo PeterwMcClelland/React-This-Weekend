b92b37e5de1f200d603aa4c2f0e4c5090b37c679
Peter McClelland <peterwmcclelland@gmail.com>
2022-11-14 19:15:53 -0600

    add columns

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,11 +5,15 @@ import React from 'react';
 function Card({person}) {
   return(
     <div className="spots-container">
+      <div className='column2'>
       <img className="spot-img" alt={person.name} src={process.env.PUBLIC_URL + person.imgPath} />
+      </div>
+      <div className='column1'>
       <div className='card-text'>
         <h2>{person.name}</h2>
         <p>{person.info}</p>
         <p>{person.address}</p>
+      </div>
       </div>
     </div>
   );
