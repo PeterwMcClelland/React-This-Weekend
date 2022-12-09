bc5bcaf8b9cd7edc2e86e10e0504255527895d5e
Peter McClelland <peterwmcclelland@gmail.com>
2022-12-08 20:32:19 -0600

    responsive text size iphone

--- src/components/Search.js
+++ src/components/Search.js
@@ -44,15 +44,29 @@ function Search({ details }) {
         <h2 className="f2">This Weekend</h2>
         
       </div>
+
+      
+
       <div className="">
-        
         <input 
           className="search-bar"
           type = "search" 
           placeholder = "Ledges, Stairs, Skate Park..."
           onChange = {handleChange}
         />
+
+{/* <div className="dropdown">
+  <button className="dropbtn">City</button>
+  <div className="dropdown-content">
+    <a href="#Portland">Portland, OR</a>
+    <a href="#Austin">Austin, TX</a>
+    <a href="#LA">Los Angeles, CA</a>
+  </div>
+  </div> */}
       </div>
+
+      
+      
       {searchList()}
     </section>
   );
