12fc6816fc5ab86e9def9bfb2bfeb39c6e8a1f28
Peter McClelland <peterwmcclelland@gmail.com>
2022-12-10 17:13:22 -0600

    new spot

--- src/data/initialDetails.js
+++ src/data/initialDetails.js
@@ -1,5 +1,4 @@
 
-
 const initialDetails = [
     {
        id: 1,
@@ -289,6 +288,13 @@ const initialDetails = [
         info: "It is a barrier to drop in the middle of the park.",
         address: "200 SW Pedestrian Trail, Portland, OR 97201, USA",
     },
+    // {
+    //     id: ,
+    //     imgPath: "./assets/img/",
+    //     name: "",
+    //     info: "",
+    //     address: "",
+    // },
     // {
     //     id: ,
     //     imgPath: "./assets/img/",
